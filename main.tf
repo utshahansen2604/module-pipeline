6e0eb3df0c48a960c307938727f38a8c790d7c42
Utshahan Sen <utshahansen2604@gmail.com>
2019-11-21 11:31:46 +0530

    Added join

--- main.tf
+++ main.tf
@@ -5,14 +5,13 @@ module "uts-pipe1" {
 }
 
 variable "my-text" {
+  //type= string
   default= "who is really awesome"
 }
 
 output "out-pipe1" {
-  value = module.uts-pipe1.Abhi-OutPipe + var.my-text
+  value = join(" ",[module.uts-pipe1.Abhi-OutPipe, var.my-text])
 }
-
-
 
 # module "uts-pipe1" {
 #   source = "github.com/Abhishek5340/Abhishek5340-terraform"
